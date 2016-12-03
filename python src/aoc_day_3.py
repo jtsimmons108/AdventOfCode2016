b5bbd6c5326b6df7fa082b107beaad297c8208fb
jtsimmons108 <jsimmons@psdr3.org>
2016-12-03 00:11:31 -0600

    Finished Day 3, 25th on part 1

--- python src/aoc_day_3.py
+++ python src/aoc_day_3.py
@@ -3,4 +3,28 @@ import itertools
 start_file = open('/Users/jeremiahsimmons/Desktop/AOC_2016/res/aoc_day_3_input.txt')
 instructions = [line for line in start_file.read().strip().splitlines()]
 
-print(instructions)
+
+def part_one():
+    total = 0
+    for instruction in instructions:
+        sides = sorted([int(x) for x in instruction.split()])
+        if sides[0] + sides[1] > sides[2]:
+            total += 1
+    return total
+
+def part_two():
+    total = 0
+    for i in range(0, len(instructions), 3):
+        first = [int(x) for x in instructions[i].split()]
+        second = [int(x) for x in instructions[i + 1].split()]
+        third = [int(x) for x in instructions[i+ 2].split()]
+        for i in range(3):
+            sides = sorted([first[i], second[i], third[i]])
+            if sides[0] + sides[1] > sides[2]:
+                total += 1
+
+    return total
+
+print('Part one:', part_one())
+print('Part two:', part_two())
+
