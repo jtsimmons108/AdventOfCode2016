c3ba6e94175f1bad5893b9d2834447ff3781ed3a
jtsimmons108 <jsimmons@psdr3.org>
2016-12-06 14:36:47 -0600

    Finished Day 6

---
+++ python src/aoc_day_6.py
@@ -0,0 +1,20 @@
+from __future__ import print_function
+from collections import Counter
+
+start_file = open('../res/aoc_day_6_input.txt')
+instructions = start_file.read().strip().splitlines()
+
+words = {}
+for line in instructions:
+    for i in range(len(line)):
+        words.setdefault(i, [])
+        words[i].append(line[i])
+
+result1 = ''
+result2 = ''
+for i in words.keys():
+    result1 += Counter(words[i]).most_common()[0][0]
+    result2 += Counter(words[i]).most_common()[-1][0]
+
+print(result1)
+print(result2)
