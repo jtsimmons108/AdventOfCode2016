f461234ee60f512c40b860dbc722b298c1eafe57
jtsimmons108 <jsimmons@psdr3.org>
2016-12-10 00:12:15 -0600

    Cleaned up day 10

--- python src/leaderbord_display.py
+++ python src/leaderbord_display.py
@@ -8,12 +8,14 @@ for member, stars in sorted([(name, int(members[name]['stars'])) for name in mem
     user = members[member]
     print(user['name'], ':', stars, '*')
     daily_info = members[member]['completion_day_level']
-    for day in sorted(daily_info.keys()):
+    for day in sorted(daily_info.keys(), key = lambda x: int(x)):
         part_one_time_string, part_two_time_string = '',''
         if '1' in daily_info[day].keys():
             part_one_time_string = dateutil.parser.parse(daily_info[day]['1']['get_star_ts']).strftime('%I:%M:%S %p %m/%d/%y')
         if '2' in daily_info[day].keys():
             part_two_time_string = dateutil.parser.parse(daily_info[day]['2']['get_star_ts']).strftime('%I:%M:%S %p %m/%d/%y')
+        if len(day) == 1:
+            day = '0' + day
         print('Day', day, ':  Part 1: ', part_one_time_string, '\tPart 2:', part_two_time_string)
     print('\n')
 
